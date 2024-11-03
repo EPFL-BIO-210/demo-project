924ea46cbe01a76e0d80aac23157c7326a9ebdc9
AlexEMG <alexander@deeplabcut.org>
2024-11-03 20:12:38 +0100

    Included doctests and unittests, updated readme to show how to use them

--- LotkaVolterraModel.py
+++ LotkaVolterraModel.py
@@ -44,3 +44,102 @@ def dX_dt(X, a=1.0, b=0.1, c=1.5, d=0.75):
     return np.array([a * X[0] - b * X[0] * X[1], -c * X[1] + d * b * X[0] * X[1]])
 
 
+def d2X_dt2(X, a=1.0, b=0.1, c=1.5, d=0.75):
+    """
+    Compute the Jacobian matrix evaluated at X.
+
+    Parameters
+    ----------
+
+    X : array
+        Vector state of populations (rabbits, foxes)
+    a : float, optional
+        natural growth rate of the prey (rabbit)
+    b : float, optional
+        natural dying rate of the prey
+    c : float, optional
+        natural growth rate of the predator (fox)
+    d : float, optional
+        natural dying rate of the predator
+
+    Returns
+    -------
+    numpy array, shape (2, 2)
+        Array containing Jacobian of dX_dt
+
+
+    Example
+    -------
+    Jacobian at [0, 0] for 0 rabbits and 0 foxes for default parameters
+    >>> d2X_dt2(np.zeros(2))
+    array([[ 1. , -0. ],
+           [ 0. , -1.5]])
+    >>> d2X_dt2(np.zeros(2),a=2,b=.3,c=1.2,d=0.4)
+    array([[ 2. , -0. ],
+           [ 0. , -1.2]])
+    """
+    return np.array([[a - b * X[1], -b * X[0]], [b * d * X[1], -c + b * d * X[0]]])
+
+
+def population_equilibrium(a=1.0, b=0.1, c=1.5, d=0.75):
+    """
+    Returns equilibrium points of dX_dt, i.e. where growth rate is equal to zero.
+
+    Parameters
+    ----------
+
+    a : float, optional
+        natural growth rate of the prey (rabbit)
+    b : float, optional
+        natural dying rate of the prey
+    c : float, optional
+        natural growth rate of the predator (fox)
+    d : float, optional
+        natural dying rate of the predator
+
+    Returns
+    -------
+    list
+        List of points with dX_dt = 0.
+
+    Examples
+    -------
+    >>> population_equilibrium()
+    (array([0., 0.]), array([20., 10.]))
+    >>> population_equilibrium(1,0.1,1.5,.2)   
+    (array([0., 0.]), array([75., 10.]))
+    """
+    return np.zeros(2), np.array([c / (d * b), a / b])
+
+
+def check_equilibrium(a=1.0, b=0.1, c=1.5, d=0.75):
+    """
+    Checks if population_equilibrium returns fixpoints of dX_dt.
+
+    Parameters
+    ----------
+
+    a : float, optional
+        natural growth rate of the prey (rabbit)
+    b : float, optional
+        natural dying rate of the prey
+    c : float, optional
+        natural growth rate of the predator (fox)
+    d : float, optional
+        natural dying rate of the predator
+
+    """
+
+    equilibria = population_equilibrium(a=a, b=b, c=c, d=d)
+    for eq in equilibria:
+        if np.all(dX_dt(eq, a=a, b=b, c=c, d=d) == np.zeros(2)):
+            print(eq, " is a fix point!")
+        else:
+            print(eq, " is not a fix point!")
+
+
+if __name__ == "__main__":
+    import doctest
+
+    print("Starting doctests")  # not required!
+    doctest.testmod()
